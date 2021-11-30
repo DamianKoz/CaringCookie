2b414c63b1b3d285c4b007b662ebc65563fa39fb
Damian Kozakiewicz <damiankozakiewicz@MacBook-Air-von-Damian.local>
2021-11-30 20:25:02 +0100

    Created Blog Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
+from typing import ContextManager
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    date_published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,7 @@
-import django
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.list_blogs, name="blog_list"),
+    path("blog/<int:pk>", views.detail_view, name="blogs_detail")
+]
