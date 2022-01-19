040554068861e6652a28b15fdd72044d56313a9f
Damian <damian.makoza@gmail.com>
2022-01-19 15:00:37 +0100

    Changed Design of the Homepage and the blogs. Added a not yet working view for creating new blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render, get_list_or_404
 from . models import Blog
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,3 +13,6 @@ def list_blogs(request):
 def detail_view(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     return render(request, "blog/detail.html", {"blog": blog})
+
+def create_blog(request, user):
+    return render(request, "blog/create_blog.html", {"user": user})
