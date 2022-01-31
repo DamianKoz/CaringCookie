e4472353f449b3d6d0b63fea905fd42800668837
Maike <maike.appenzeller@gmail.com>
2022-01-31 15:58:19 +0100

    Beitragsbearbeitung erstellt

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.list_blogs, name="blog_list"),
     path("blog/<int:pk>", views.detail_view, name="blogs_detail"),
-    path("create_blog", views.create_blog, name="create_blog")
+    path("create_blog", views.create_blog, name="create_blog"),
+    path("change_blog/<int:pk>", views.change_blog, name="change_blog")
 ]
