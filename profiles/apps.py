aa130a173efb0a8db3d645e74dfea9664b5c84cc
Chen <Chen_Chen.Ye@edu.fhdw.de>
2022-02-03 14:21:55 +0100

    Profil wird automatisch mit User erstellt

--- profiles/apps.py
+++ profiles/apps.py
@@ -10,6 +10,3 @@ class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
-    # add this
-    def ready(self):
-        import profiles.signals  # noqa
